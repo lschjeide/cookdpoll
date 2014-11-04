3db589e7d95b9cb23a23c7e2051e3f5ef380696e
Leif <lschjeide@gmail.com>
2014-11-04 14:44:13 +1100

    update dns script modification

--- update_dns.sh
+++ update_dns.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-echo $1
-echo $2
 
-echo aws ec2 describe-instances   --region=ap-southeast-2   --filter "Name=tag:Name,Values=$1-$2"   --query='Reservations[*].Instances[*].PublicDnsName'   --output=text
+echo aws ec2 describe-instances   --region=ap-southeast-2   --filter "Name=tag:Name,Values=$JOB_NAME-$BUILD_NUMBER"   --query='Reservations[*].Instances[*].PublicDnsName'   --output=text
 
-AWS_DNS_NAME=$(aws ec2 describe-instances   --region=ap-southeast-2   --filter "Name=tag:Name,Values=$1-$2"   --query='Reservations[*].Instances[*].PublicDnsName'   --output=text)
+AWS_DNS_NAME=$(aws ec2 describe-instances   --region=ap-southeast-2   --filter "Name=tag:Name,Values=$JOB_NAME-$BUILD_NUMBER"   --query='Reservations[*].Instances[*].PublicDnsName'   --output=text)
 echo $AWS_DNS_NAME
 
 
@@ -15,10 +13,10 @@ EOF`
 
 echo $VARIABLE3
 
-echo $VARIABLE3 > /tmp/change_resource.json
+echo $VARIABLE3 > $WORKSPACE/change_resource.json
 
-echo aws route53 change-resource-record-sets --hosted-zone-id Z3MBDC08G5UXLE --change-batch file:///tmp/change_resource.json
+echo aws route53 change-resource-record-sets --hosted-zone-id Z3MBDC08G5UXLE --change-batch file://$WORKSPACE/change_resource.json
 
-OUTPUT=$(aws route53 change-resource-record-sets --hosted-zone-id Z3MBDC08G5UXLE --change-batch file:///tmp/change_resource.json)
+OUTPUT=$(aws route53 change-resource-record-sets --hosted-zone-id Z3MBDC08G5UXLE --change-batch file://$WORKSPACE/change_resource.json)
 
 echo $OUTPUT
