e617856e21cb745d8e894b021f6ac12d6271bfd5
Leif <lschjeide@gmail.com>
2014-11-04 16:26:20 +1100

    update dns script terminate old instances

--- update_dns.sh
+++ update_dns.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/sh 
+set -x
 
 
-echo aws ec2 describe-instances   --region=ap-southeast-2   --filter "Name=tag:Name,Values=$JOB_NAME-$BUILD_NUMBER"   --query='Reservations[*].Instances[*].PublicDnsName'   --output=text
+OLD_INSTANCE_ID=$(aws ec2 describe-instances   --region=ap-southeast-2   --filter "Name=tag:Live,Values=true"   --query='Reservations[*].Instances[*].InstanceId'   --output=text)
+echo $OLD_INSTANCE_ID
+
+VAGRANT_LOG=debug AWSNAME=${JOB_NAME}-${BUILD_NUMBER} vagrant up --provider=aws
+
 
 AWS_DNS_NAME=$(aws ec2 describe-instances   --region=ap-southeast-2   --filter "Name=tag:Name,Values=$JOB_NAME-$BUILD_NUMBER"   --query='Reservations[*].Instances[*].PublicDnsName'   --output=text)
 echo $AWS_DNS_NAME
@@ -15,8 +20,12 @@ echo $VARIABLE3
 
 echo $VARIABLE3 > $WORKSPACE/change_resource.json
 
-echo aws route53 change-resource-record-sets --hosted-zone-id Z3MBDC08G5UXLE --change-batch file://$WORKSPACE/change_resource.json
-
 OUTPUT=$(aws route53 change-resource-record-sets --hosted-zone-id Z3MBDC08G5UXLE --change-batch file://$WORKSPACE/change_resource.json)
 
 echo $OUTPUT
+
+aws ec2 terminate-instances --instance-ids $OLD_INSTANCE_ID
+
+rm -rf .vagrant
+
+vagrant global-status --prune
