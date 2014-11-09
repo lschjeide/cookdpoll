28665fc77a01cd21773db43864350b4e261fbf10
Leif <lschjeide@gmail.com>
2014-11-10 10:50:15 +1100

    fixed script for register clean with elb

--- register_clean.sh
+++ register_clean.sh
@@ -1,11 +1,11 @@
 #!/bin/sh 
 set -x
 
-register_clean.shNEW_INSTANCE_ID=$(aws ec2 describe-instances   --region=ap-southeast-2   --filter "Name=tag:Name,Values=$JOB_NAME-$BUILD_NUMBER"   --query='Reservations[*].Instances[*].InstanceId'   --output=text)
+NEW_INSTANCE_ID=$(aws ec2 describe-instances   --region=ap-southeast-2   --filter "Name=tag:Name,Values=$JOB_NAME-$BUILD_NUMBER"   --query='Reservations[*].Instances[*].InstanceId'   --output=text)
 
-register-instances-with-load-balancer --load-balancer-name testdpoll2 --instances $NEW_INSTANCE_ID
+aws elb register-instances-with-load-balancer --load-balancer-name testdpoll2 --instances $NEW_INSTANCE_ID
 
-deregister-instances-from-load-balancer --load-balancer-name testdpoll2 --instances $OLD_INSTANCE_ID
+aws elb deregister-instances-from-load-balancer --load-balancer-name testdpoll2 --instances $OLD_INSTANCE_ID
 
 rm -rf .vagrant
 
