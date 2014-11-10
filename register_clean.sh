364b2a4842df61cedeeb3e8df62329a549e05525
Leif <lschjeide@gmail.com>
2014-11-11 10:32:11 +1100

    production manuevering

--- register_clean.sh
+++ register_clean.sh
@@ -3,11 +3,11 @@ set -x
 
 NEW_INSTANCE_ID=$(aws ec2 describe-instances   --region=ap-southeast-2   --filter "Name=tag:Name,Values=$JOB_NAME-$BUILD_NUMBER"   --query='Reservations[*].Instances[*].InstanceId'   --output=text)
 
-aws elb register-instances-with-load-balancer --load-balancer-name testdpoll2 --instances $NEW_INSTANCE_ID
+aws elb register-instances-with-load-balancer --load-balancer-name $ELB --instances $NEW_INSTANCE_ID
 
-OLD_INSTANCE_ID=$(aws ec2 describe-instances   --region=ap-southeast-2   --filter "Name=tag:Live,Values=false"   --query='Reservations[*].Instances[*].InstanceId'   --output=text)
+OLD_INSTANCE_ID=$(aws ec2 describe-instances   --region=ap-southeast-2   --filter "Name=tag:Live,Values=$DEADVAL"   --query='Reservations[*].Instances[*].InstanceId'   --output=text)
 
-aws elb deregister-instances-from-load-balancer --load-balancer-name testdpoll2 --instances $OLD_INSTANCE_ID
+aws elb deregister-instances-from-load-balancer --load-balancer-name $ELB --instances $OLD_INSTANCE_ID
 
 rm -rf .vagrant
 
