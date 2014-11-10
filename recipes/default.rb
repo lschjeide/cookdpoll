5ff828faece9813a59d7ee00318267d39fc91140
Tristan Morgan <tmorgan@dius.com.au>
2014-11-10 11:44:11 +1100

    exec permission for the directory

--- recipes/default.rb
+++ recipes/default.rb
@@ -84,7 +84,7 @@ end
 execute "bundle config build.nokogiri --use-system-libraries"
 
 directory '/home/ec2-user/unicorn' do 
-	mode 0644
+	mode 0755
 	owner 'ec2-user'
 	group 'ec2-user'
 	action :create
