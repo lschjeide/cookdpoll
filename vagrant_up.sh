54f2ee7c6c3d90d0c40ee1edfa35ecd831499506
Leif <lschjeide@gmail.com>
2014-11-10 11:53:32 +1100

    Typo on old tag

--- vagrant_up.sh
+++ vagrant_up.sh
@@ -4,6 +4,6 @@ set -x
 
 OLD_INSTANCE_ID=$(aws ec2 describe-instances   --region=ap-southeast-2   --filter "Name=tag:Live,Values=$LIVEVAL"   --query='Reservations[*].Instances[*].InstanceId'   --output=text)
 
-aws ec2 create-tags --resources OLD_INSTANCE_ID --tags Key=Old,Value=true
+aws ec2 create-tags --resources $OLD_INSTANCE_ID --tags Key=Old,Value=true
 
 VAGRANT_LOG=debug AWSNAME=${JOB_NAME}-${BUILD_NUMBER} vagrant up --provider=aws
