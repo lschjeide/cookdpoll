f724e2add780cac7cfadb121156661a607434ecf
Leif <lschjeide@gmail.com>
2014-11-11 14:33:54 +1100

    wait until inservice and terminate old instances

--- register_clean.sh
+++ register_clean.sh
@@ -7,7 +7,18 @@ aws elb register-instances-with-load-balancer --load-balancer-name $ELB --instan
 
 OLD_INSTANCE_ID=$(aws ec2 describe-instances   --region=ap-southeast-2   --filter "Name=tag:Live,Values=$DEADVAL"   --query='Reservations[*].Instances[*].InstanceId'   --output=text)
 
+HEATH_CHECK=$(aws elb describe-instance-health --load-balancer-name $ELB  --instances $NEW_INSTANCE_ID)
+
+until [ `echo $HEATH_CHECK | grep -o "InService"` ]
+do
+	echo $HEATH_CHECK
+	sleep 5
+	HEATH_CHECK=$(aws elb describe-instance-health --load-balancer-name $ELB  --instances $NEW_INSTANCE_ID)
+done
+
 aws elb deregister-instances-from-load-balancer --load-balancer-name $ELB --instances $OLD_INSTANCE_ID
+
+aws ec2 terminate-instances --instance-ids $OLD_INSTANCE_ID
 
 rm -rf .vagrant
 
