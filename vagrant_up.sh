53e890c15bdfb2ecb3a0019353aa9f0872d1fd84
Leif <lschjeide@gmail.com>
2014-11-13 12:35:17 +1100

    auto dns elb

--- vagrant_up.sh
+++ vagrant_up.sh
@@ -1,6 +1,15 @@
 #!/bin/sh 
 set -x
 
+ELB_HOSTNAME=$(aws elb create-load-balancer --load-balancer-name $ELB --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --availability-zones ap-southeast-2b --security-groups sg-b5ce00d0)
+
+VARIABLE3=`cat <<EOF
+{"Comment": "CLI Create route53 resource","Changes": [{"Action": "CREATE","ResourceRecordSet": {"Name": "$DNSNAME.test.dius.com.au","Type": "CNAME","TTL": 300,"ResourceRecords": [{"Value": "$ELB_HOSTNAME"}]}}]}
+EOF`
+
+echo $VARIABLE3 > $WORKSPACE/change_resource.json
+
+aws route53 change-resource-record-sets --hosted-zone-id Z3MBDC08G5UXLE --change-batch file://$WORKSPACE/change_resource.json
 
 OLD_INSTANCE_ID=$(aws ec2 describe-instances   --region=ap-southeast-2   --filter "Name=tag:Live,Values=$LIVEVAL"   --query='Reservations[*].Instances[*].InstanceId'   --output=text)
 
